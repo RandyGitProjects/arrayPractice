9351d37a2af6b13444a7dd8b2991686694a81340
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-06 22:10:18 -0700

    Show three different array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 var arrayStrings = ["Dark Knight", "The Other Guys", "Jurrasic Park", "Bram Stokers Dracula"];
 var arrayNumbers = [32, 32, 13, 9];
 var arrayBooleans = [true, false, false, false, true];
+
+// This method will remove the first element in the chosen array.
+console.log(arrayBooleans.shift())
+// This method will remove the last element in the chosen array.
+console.log(arrayNumbers.pop())
+// This method will add a new element to end of the chosen array.
+console.log(arrayStrings.push ("Star Wars"))
