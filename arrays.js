c7440e1200109bd7bce6d4c668d3cf1b24a521a8
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-06 21:38:15 -0700

    Declare three variables for arrays

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var arrayStrings = ["Dark Knight", "The Other Guys", "Jurrasic Park", "Bram Stokers Dracula"];
+var arrayNumbers = [32, 32, 13, 9];
+var arrayBooleans = [true, false, false, false, true];
